86ff06c33c094669194def7cb2492095d608637e
Rishav <iamrishavkr@gmail.com>
2024-05-15 15:05:29 +0530

    added to card

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const ecommerce = useSelector((state) => state.ecommerce);
 
-export default Cart
+  const isPresentInCart = (id) => {
+    for (let i = 0; i < ecommerce.cart.length; i++) {
+      if (ecommerce.cart[i] === id) {
+        return true;
+      }
+    }
+    return false;
+  };
+  console.log(ecommerce);
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      {ecommerce.product ? (
+        ecommerce.product.map((item) => {
+          return isPresentInCart(item.id) ? (
+            <div
+              className="item-box px-2 py-1 shadow-lg flex justify-center items-center flex-col"
+              key={item.id}
+            >
+              <div className="img-box h-56 w-52">
+                <img
+                  src={item.images[0]}
+                  alt=""
+                  className="h-full w-full object-cover"
+                />
+              </div>
+              <p className="title">{item.title}</p>
+              <p className="price">${item.price}</p>
+              <div className="flex justify-between gap-4 ">
+                <button className="bg-black text-white px-4 py-2 rounded-lg">
+                  Wishlist
+                </button>
+                <button className="bg-black text-white  px-4 py-2 rounded-lg">
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+          ) : (
+            null
+          );
+        })
+      ) : (
+        <div>No product added yet</div>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
--- src/Pages/Mens.jsx
+++ src/Pages/Mens.jsx
@@ -1,20 +1,26 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addToProduct } from "../store/productSlice";
+import { addToCart } from "../store/productSlice";
 const Mens = () => {
-  const [udata, setUdata] = useState([]);
+  const [udata, setUdata] = useState(null);
   const dispatch = useDispatch();
+
   useEffect(() => {
-    axios.get("https://api.escuelajs.co/api/v1/products").then((response) => {
-      console.log(response.data);
+      axios.get("https://api.escuelajs.co/api/v1/products").then((response) => {
       setUdata(response.data);
       dispatch(addToProduct(response.data));
     });
   }, []);
+
+  const AddToCart = (id)=>{
+    dispatch(addToCart(id));
+  }
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-      {udata.map((item) => {
+      {udata 
+        ?udata.map((item) => {
         return (
           <div
             className="item-box px-2 py-1 shadow-lg flex justify-center items-center flex-col"
@@ -33,13 +39,17 @@ const Mens = () => {
               <button className="bg-black text-white px-4 py-2 rounded-lg">
                 Wishlist
               </button>
-              <button className="bg-black text-white  px-4 py-2 rounded-lg">
+              <button className="bg-black text-white  px-4 py-2 rounded-lg"
+              onClick={()=>AddToCart(item.id)}
+              >
                 Add to Cart
               </button>
             </div>
           </div>
         );
-      })}
+      })
+      :null
+    }
     </div>
   );
 };
